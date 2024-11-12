897fbd4167df5d1f48354f2e2435b000a93ed770
2sandeep7 <ssandeep8696@gmail.com>
2024-11-13 01:09:31 +0530

    added

--- frontend/src/util/axios.js
+++ frontend/src/util/axios.js
@@ -1,5 +1,5 @@
 import axiosInstance from 'axios';
 
 export const axios = axiosInstance.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://schedulot.onrender.com/api',
     });
